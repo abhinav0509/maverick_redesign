20d3b23365adf544aedf8d1021dfaeebc79b48e7
Abhinav Raj <abhinav.raj0509@gmail.com>
2024-08-02 19:15:46 +0530

    added js dependency libraries

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,27 +20,58 @@
 //     }
 //     updateCount();
 // })
- import {CountUp} from "https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.0.7/countUp.js"
+// import {CountUp} from "https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.0.7/countUp.js"
 
- function startCount(){
-    const $counters=document.querySelectorAll('.counter'),
-    options={
-        useEasing:true,
-        useGrouping:true,
-        separator:",",
-        decimal:"."
-    };
-    $counters.forEach((item)=>{
-        const value=item.dataset.value;
-        const counter=new CountUp(item,value,options);
-        counter.start();
-    })
- }
+//  function startCount(){
+//     const $counters=document.querySelectorAll('.counter'),
+//     options={
+//         useEasing:true,
+//         useGrouping:true,
+//         separator:",",
+//         decimal:"."
+//     };
+//     $counters.forEach((item)=>{
+//         const value=item.dataset.value;
+//         const counter=new CountUp(item,value,options);
+//         counter.start();
+//     })
+//  }
 
- new Waypoint({
-    element:document.querySelector('.level'),
-    handler:function(){
-        countStart()
-    },
-    offset:'50%'
- });
+//  new Waypoint({
+//     element:document.querySelector('.level'),
+//     handler:function(){
+//         startCount();                                
+//     },
+//     offset:'50%'
+//  });
+
+import { CountUp } from "https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.0.7/countUp.js";
+
+
+
+function countStart(){
+  const $counters = document.querySelectorAll(".js-count-up"),
+        options = {
+          useEasing: true,
+          useGrouping: true,
+          separator: ",",
+          decimal: "."
+        };
+  console.log("inside");      
+
+  $counters.forEach( (item) => {
+    const value = item.dataset.value;
+    const counter = new CountUp(item, value, options);
+
+    counter.start();
+  });
+}
+
+new Waypoint({
+  element: document.querySelector('.level'),
+  handler: function() {
+    countStart()
+    //this.destroy() //for once
+  },
+  offset: '50%'
+});
