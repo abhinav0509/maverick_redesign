25af2635d0e1030a082ea36daeecec5fc1325a79
Abhinav Raj <abhinav.raj0509@gmail.com>
2024-08-02 11:29:05 +0530

    minor fixes to responsivness for hero text

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,46 @@
-const counters=document.querySelectorAll('.counter');
-const speed=200;
+// const counters=document.querySelectorAll('.counter');
+// const speed=200;
 
-counters.forEach(counter=>{
-    const updateCount=()=>{
-        const target=+counter.getAttribute('data-target');   //convert string to number by add +;
-        const count=+counter.innerText;
+// counters.forEach(counter=>{
+//     const updateCount=()=>{
+//         const target=+counter.getAttribute('data-target');   //convert string to number by add +;
+//         const count=+counter.innerText;
 
-        const increment=target/speed;
+//         const increment=target/speed;
 
-        if(count<target){
-            counter.innerHTML=count+increment;
-            setTimeout(updateCount,1);
-        }
-        else{
-            count.innerText=target;
-        }
+//         if(count<target){
+//             counter.innerHTML=count+increment;
+//             setTimeout(updateCount,1);
+//         }
+//         else{
+//             count.innerText=target;
+//         }
 
      
-    }
-    updateCount();
-})
+//     }
+//     updateCount();
+// })
+ import {CountUp} from "https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.0.7/countUp.js"
+
+ function startCount(){
+    const $counters=document.querySelectorAll('.counter'),
+    options={
+        useEasing:true,
+        useGrouping:true,
+        separator:",",
+        decimal:"."
+    };
+    $counters.forEach((item)=>{
+        const value=item.dataset.value;
+        const counter=new CountUp(item,value,options);
+        counter.start();
+    })
+ }
+
+ new Waypoint({
+    element:document.querySelector('.level'),
+    handler:function(){
+        countStart()
+    },
+    offset:'50%'
+ });
