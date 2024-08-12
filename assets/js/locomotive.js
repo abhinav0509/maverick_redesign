9d4cba97d52886ce9a78772586111f7a4afa2ac4
Abhinav Raj <abhinav.raj0509@gmail.com>
2024-08-12 12:04:59 +0530

    code refactoring

---
+++ assets/js/locomotive.js
@@ -0,0 +1,14 @@
+const scroll = new LocomotiveScroll({
+  el: document.querySelector("body"),
+  smooth: true,
+});
+
+// function circleMouseFollower() {
+//   window.addEventListener("mousemove", function (dets) {
+//     this.document.querySelector(
+//       ".minicircle"
+//     ).style.transform = `translate(${dets.clientX}px,${dets.clientY}px)`;
+//   });
+// }
+// circleMouseFollower();
+
